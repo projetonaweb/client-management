29f402b9ba6d3ed26bc2eeaa9dfb3339ce94853e
FOXXX <75998678+devrodrigomolina@users.noreply.github.com>
2022-11-22 23:34:00 -0300

    Criado pagina home

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Header from '../../components/Header/Header'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <Header />
+    </div>
   )
 }
 
