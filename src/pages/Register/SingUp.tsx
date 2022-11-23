1eecae702bcd75110fc9a8583066af254a36f0f7
FOXXX <75998678+devrodrigomolina@users.noreply.github.com>
2022-11-22 23:33:47 -0300

    Adicionado novos inputs

--- src/pages/Register/SingUp.tsx
+++ src/pages/Register/SingUp.tsx
@@ -3,61 +3,91 @@ import { Inputs } from "../../components/Inputs/Inputs";
 import { Title } from "../../components/Title/Title";
 import * as C from "./style";
 import { useState } from "react";
+import Buttons from "../../components/Buttons/Buttons";
 
 const SingUp = () => {
-  const [name, setName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [email, setEmail] = useState("");
-  const [cep, setCep] = useState("");
+  const [name, setName] = useState<String>("");
+  const [lastName, setLastName] = useState<String>("");
+  const [email, setEmail] = useState<String>("");
+  const [password, setPasssword] = useState<String>("");
+  const [passwordConfirm, setPassswordConfirm] = useState<String>("");
+  const [cep, setCep] = useState<String>("");
 
   return (
-    <C.ContainerSingUp>
-      <Title message="Digite seus dados para se cadastrar" />
- 
-      <Inputs
-        type="text"
-        id="name"
-        name="Nome"
-        value={name}
-        placeholder="Digite seu nome"
-        handleOnChange={({ target }: React.ChangeEvent<HTMLInputElement>) =>
-        setName(target.value)
-      }
-      />
+    <C.ContainerRegister>
+      <C.ContainerSingUp>
+        <Title message="Digite seus dados para se cadastrar" />
 
-      <Inputs
-        type="text"
-        id="lastname"
-        name="Sobrenome"
-        value={lastName}
-        placeholder="Digite seu sobrenome"
-        handleOnChange={({ target }: React.ChangeEvent<HTMLInputElement>) =>
-          setLastName(target.value)
-        }
-      />
+        <Inputs
+          type="text"
+          id="name"
+          name="Nome"
+          value={name}
+          placeholder="Digite seu nome"
+          handleOnChange={({ target }: React.ChangeEvent<HTMLInputElement>) =>
+            setName(target.value)
+          }
+        />
 
-      <Inputs
-        type="text"
-        id="email"
-        name="Email"
-        value={email}
-        placeholder="Digite seu email"
-        handleOnChange={({ target }: React.ChangeEvent<HTMLInputElement>) =>
-          setEmail(target.value)
-        }
-      />
+        <Inputs
+          type="text"
+          id="lastname"
+          name="Sobrenome"
+          value={lastName}
+          placeholder="Digite seu sobrenome"
+          handleOnChange={({ target }: React.ChangeEvent<HTMLInputElement>) =>
+            setLastName(target.value)
+          }
+        />
 
-      <Inputs
-        type="text"
-        id="cep"
-        name="CEP"
-        value={cep}
-        placeholder="Digite seu CEP"
-        handleOnChange={({ target }: React.ChangeEvent<HTMLInputElement>) =>
-          setCep(target.value)
-        }
-      />
-    </C.ContainerSingUp>
+        <Inputs
+          type="text"
+          id="email"
+          name="Email"
+          value={email}
+          placeholder="Digite seu email"
+          handleOnChange={({ target }: React.ChangeEvent<HTMLInputElement>) =>
+            setEmail(target.value)
+          }
+        />
+
+        <Inputs
+          type="password"
+          id="senha"
+          name="Senha"
+          value={password}
+          placeholder="Digite sua senha"
+          handleOnChange={({ target }: React.ChangeEvent<HTMLInputElement>) =>
+            setPasssword(target.value)
+          }
+        />
+        <Inputs
+          type="password"
+          id="senha"
+          name="Repita sua senha"
+          value={passwordConfirm}
+          placeholder="Digite sua senha novamente"
+          handleOnChange={({ target }: React.ChangeEvent<HTMLInputElement>) =>
+            setPassswordConfirm(target.value)
+          }
+        />
+
+        <Inputs
+          type="text"
+          id="cep"
+          name="CEP"
+          value={cep}
+          placeholder="Digite seu CEP"
+          handleOnChange={({ target }: React.ChangeEvent<HTMLInputElement>) =>
+            setCep(target.value)
+          }
+        />
+        <div className="botao">
+          <Buttons text="Enviar" color="#239c058d"
+          color2="red" />
+        </div>
+      </C.ContainerSingUp>
+    </C.ContainerRegister>
   );
 };
 
