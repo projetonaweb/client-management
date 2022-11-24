2ce6c1f4ff010c4b216aff04655ee91b94770ab0
FOXXX <75998678+devrodrigomolina@users.noreply.github.com>
2022-11-24 00:15:46 -0300

    Criado pagina home (Parcialmente)

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,11 +1,31 @@
 import React from 'react'
+import Cards from '../../components/Cards/Cards'
 import Header from '../../components/Header/Header'
+import { Title } from '../../components/Title/Title'
+import * as C from './style'
 
 const Home = () => {
+  const name = 'Rodrigo'
+
   return (
-    <div>
-      <Header />
-    </div>
+    <C.ContainerHome>
+      <div className="header">
+        <Header />
+      </div>
+
+      <C.ContainerPanel>
+        <div className="titulo">
+          <Title message={`Bem vindo ${name}`} />
+          <h4 className='date'>23 nov 2022</h4>
+        </div>
+        <div className="cards">
+          <Cards text="Clientes cadastrados" quantidade="30"/>
+          <Cards text="Clientes maconheiros" quantidade="24"/>
+          <Cards text="Clientes macumbeiros" quantidade="13"/>
+        </div>
+      </C.ContainerPanel>
+      
+    </C.ContainerHome>
   )
 }
 
--- src/pages/Home/style.tsx
+++ src/pages/Home/style.tsx
@@ -1,1 +1,29 @@
 import styled from "styled-components";
+
+export const ContainerHome = styled.div`
+  display: flex;
+  background-color: aliceblue;
+`;
+
+export const ContainerPanel = styled.div`
+  width: 100%;
+  max-width: 90%;
+  margin: 0 auto;
+  height: 100vh;
+  .titulo {
+    h1 {
+      margin-top: 30px;
+      font-size: 2.5rem;
+      color: #6b6b6bdc;
+    }
+    .date {
+      color: #6b6b6bdc;
+    }
+  }
+  .cards {
+    width: 70%;
+    margin: 40px 0;
+    display: flex;
+    justify-content: space-between;
+  }
+`;
