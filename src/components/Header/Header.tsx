c659f78540a3422506830b10b8bee8c33d6ffe64
FOXXX <75998678+devrodrigomolina@users.noreply.github.com>
2022-11-24 00:15:58 -0300

    Criado menu lateral (header)

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,25 @@
 import React from 'react'
 import * as C from './style'
+import {  GoGear } from 'react-icons/go'
+import {  BsFillPeopleFill } from 'react-icons/bs'
+import {  AiOutlinePrinter } from 'react-icons/ai'
 
 const Header = () => {
   return (
-    <C.ContainerHeader>Header</C.ContainerHeader>
+    <C.ContainerHeader>
+      <div className="user-section">
+        <div className="image-user">
+          <img src="" alt="" />
+        </div>
+        <span>UserName</span>
+      </div>
+
+      <C.HeaderContainer>
+        <BsFillPeopleFill className='icon' />
+        <AiOutlinePrinter className='icon' />
+        <GoGear className='icon' />
+      </C.HeaderContainer >
+    </C.ContainerHeader>
   )
 }
 
--- src/components/Header/style.tsx
+++ src/components/Header/style.tsx
@@ -1,7 +1,37 @@
 import styled from "styled-components";
 
 export const ContainerHeader = styled.div`
-  width: 100vw;
-  height: 80px;
-  background-color: #69666690;
+  width: 90px;
+  height: 100vh;
+  background-color: #2C74E6;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  flex-direction: column;
+  .user-section {
+    .image-user {
+      width: 65px;
+      height: 65px;
+      border-radius: 50%;
+      background-color: #9e9d9dbc;
+      border: 2px solid white;
+      margin-top: 30px;
+      margin-bottom: 7px;
+      cursor: pointer;
+    }
+  }
+`
+
+export const HeaderContainer = styled.nav`
+  width: 100%;
+  height: 400px;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  flex-direction: column;
+  .icon {
+    font-size: 2rem;
+    color: white;
+    cursor: pointer;
+  }
 `
