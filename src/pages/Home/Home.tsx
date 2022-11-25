c40854d838cffb3b4bd8e6ece0fc05e7a9acea54
FOXXX <75998678+devrodrigomolina@users.noreply.github.com>
2022-11-24 23:51:24 -0300

    Update Home.tsx

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Cards from '../../components/Cards/Cards'
+import ClientsPanel from '../../components/ClientsPanel/ClientsPanel'
 import Header from '../../components/Header/Header'
 import { Title } from '../../components/Title/Title'
 import * as C from './style'
@@ -18,10 +19,19 @@ const Home = () => {
           <Title message={`Bem vindo ${name}`} />
           <h4 className='date'>23 nov 2022</h4>
         </div>
-        <div className="cards">
-          <Cards text="Clientes cadastrados" quantidade="30"/>
-          <Cards text="Clientes maconheiros" quantidade="24"/>
-          <Cards text="Clientes macumbeiros" quantidade="13"/>
+
+        <div className="container_cards">
+          <Title message="Estatisticas" />
+          <div className="cards">
+            <Cards text="Clientes cadastrados" quantidade="30"/>
+            <Cards text="Clientes maconheiros" quantidade="24"/>
+            <Cards text="Clientes macumbeiros" quantidade="13"/>
+          </div>
+        </div>
+
+        <div className="client_panel">
+          <Title message='Clientes cadastrados' />
+          <ClientsPanel />
         </div>
       </C.ContainerPanel>
       
