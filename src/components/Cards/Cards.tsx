cc56e2998944d0966f7e0b1f2bbd1b2d99122a84
FOXXX <75998678+devrodrigomolina@users.noreply.github.com>
2022-11-24 23:51:31 -0300

    Update Cards.tsx

--- src/components/Cards/Cards.tsx
+++ src/components/Cards/Cards.tsx
@@ -1,16 +1,18 @@
 import React from 'react'
 import * as C from './style'
-
+import { Title } from '../Title/Title'
 type CardsType = {
   quantidade: string,
   text: string
 }
 const Cards = ({quantidade, text}: CardsType) => {
   return (
-    <C.ContainerCards>
-      <h1>{quantidade}</h1>
-      <p>{text}</p>
-    </C.ContainerCards>
+    <>
+      <C.ContainerCards>
+        <h1>{quantidade}</h1>
+        <p>{text}</p>
+      </C.ContainerCards>
+    </>
   )
 }
 
