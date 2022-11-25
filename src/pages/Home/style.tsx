1904e2ab5fffdf1470d2f1ae875db121bfb3c7a5
FOXXX <75998678+devrodrigomolina@users.noreply.github.com>
2022-11-24 23:51:22 -0300

    Update style.tsx

--- src/pages/Home/style.tsx
+++ src/pages/Home/style.tsx
@@ -7,23 +7,41 @@ export const ContainerHome = styled.div`
 
 export const ContainerPanel = styled.div`
   width: 100%;
-  max-width: 90%;
+  max-width: 85%;
   margin: 0 auto;
   height: 100vh;
+  .container_cards {
+    & > h1 {
+      color: #181717c1;
+      font-size: 1.5rem;
+    }
+  }
   .titulo {
     h1 {
       margin-top: 30px;
       font-size: 2.5rem;
-      color: #6b6b6bdc;
+      color: #181717c1;
     }
     .date {
-      color: #6b6b6bdc;
+      color: #575757e4;
+      font-size: 1.3rem;
+      font-weight: 300;
+      margin-bottom: 50px;
     }
   }
   .cards {
     width: 70%;
-    margin: 40px 0;
+    margin: 10px 0;
     display: flex;
     justify-content: space-between;
+  }
+
+
+  .client_panel {
+    & > h1 {
+      margin-top: 30px;
+      color: #575757e4;
+      font-size: 1.5rem;
+    }
   }
 `;
