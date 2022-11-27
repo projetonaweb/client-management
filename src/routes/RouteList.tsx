f6682ca9164ade926d39dc087cd5204f1e4faf99
FOXXX <75998678+devrodrigomolina@users.noreply.github.com>
2022-11-27 04:08:45 -0300

    Update RouteList.tsx

--- src/routes/RouteList.tsx
+++ src/routes/RouteList.tsx
@@ -1,5 +1,6 @@
 import { useRoutes } from "react-router-dom"
 import App from "../App"
+import Clients from "../pages/Clients/Clients"
 import NotFound from "../pages/Error/NotFound"
 import Home from "../pages/Home/Home"
 import SingUp from "../pages/Register/SingUp"
@@ -10,6 +11,7 @@ import { RequireAuth } from "./RequireAuth"
 export const RouteList = () => {
   return useRoutes([
     { path: '/', element: <RequireAuth><Home /></RequireAuth>},
+    { path: '/clients', element: <RequireAuth><Clients /></RequireAuth>},
     { path: '/login', element: <SingIn /> },
     { path: '/register', element: <SingUp /> },
     { path: '*', element: <NotFound /> }
