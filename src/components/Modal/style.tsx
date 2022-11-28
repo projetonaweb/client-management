4431f074dd2e3e8a21d7d0358986ec5fee44c2f6
FOXXX <75998678+devrodrigomolina@users.noreply.github.com>
2022-11-28 00:11:15 -0300

    Update style.tsx

--- src/components/Modal/style.tsx
+++ src/components/Modal/style.tsx
@@ -3,6 +3,35 @@ import styled from "styled-components";
 export const ContainerModal = styled.div`
   width: 100vw;
   height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
   background-color: #00000083;
   position: absolute;
+  z-index: 10;
+  .modal {
+    width: 1000px;
+    height: 850px;
+    border-radius: 10px;
+    background-color: white;
+    position: absolute;
+    padding: 20px 60px;
+    z-index: 20;
+    h1 {
+      color: #757575ca;
+      margin: 10px 0;
+      text-align: center;
+    }
+    .adress-client {
+      display: flex;
+      justify-content: space-between;
+      gap: 20px;
+      #cep {
+        width: 100%;
+      }
+      #number {
+         width: 30%
+      }
+    }
+  }
 `
