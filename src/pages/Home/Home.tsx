dbc0970f59a2024d09c0b81ca6f860402c228539
FOXXX <75998678+devrodrigomolina@users.noreply.github.com>
2022-12-05 00:23:25 -0300

    Adicionado data local e quantidade de clientes dinamica

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,12 +1,17 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Cards from '../../components/Cards/Cards'
 import Charts from '../../components/Charts/Charts'
 import Header from '../../components/Header/Header'
 import { Title } from '../../components/Title/Title'
+import { useFetch } from '../../hooks/useFetch'
 import * as C from './style'
 
 const Home = () => {
   const name = 'Rodrigo'
+  const clientRegisteredQtd = useFetch('/clientes')
+
+  const date = new Date()
+  const dateFormated = date.toLocaleDateString()
 
   return (
     <C.ContainerHome>
@@ -17,13 +22,13 @@ const Home = () => {
       <C.ContainerPanel>
         <div className="titulo">
           <Title message={`Bem vindo ${name}`} />
-          <h4 className='date'>23 nov 2022</h4>
+          <h4 className='date'>{dateFormated}</h4>
         </div>
 
         <div className="container_cards">
           <Title message="Estatisticas" />
           <div className="cards">
-            <Cards text="Clientes cadastrados" quantidade="30"/>
+            <Cards text="Clientes cadastrados" quantidade={clientRegisteredQtd.clients.length}/>
             <Cards text="Clientes maconheiros" quantidade="24"/>
             <Cards text="Clientes macumbeiros" quantidade="13"/>
           </div>
