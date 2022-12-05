b084bfab936b49ceb728c37a89d65abb6b6cb2b1
FOXXX <75998678+devrodrigomolina@users.noreply.github.com>
2022-12-05 00:24:01 -0300

    Update Header.tsx

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,17 +1,15 @@
-import React, { useState } from 'react'
-import * as C from './style'
-import {  GoGear } from 'react-icons/go'
-import {  BsFillPeopleFill } from 'react-icons/bs'
-import {  AiOutlinePrinter } from 'react-icons/ai'
-import {  SlGraph } from 'react-icons/sl'
-import Modal from '../Modal/Modal'
-import { Link } from 'react-router-dom'
+import React, { useState } from "react";
+import * as C from "./style";
+import { GoGear } from "react-icons/go";
+import { BsFillPeopleFill } from "react-icons/bs";
+import { AiOutlinePrinter } from "react-icons/ai";
+import { SlGraph } from "react-icons/sl";
+import Modal from "../ModalRegisterClients/ModalRegisterClients";
+import { Link } from "react-router-dom";
 
 const Header = () => {
-
   return (
     <C.ContainerHeader>
-
       <div className="user-section">
         <div className="image-user">
           <img src="" alt="" />
@@ -24,33 +22,32 @@ const Header = () => {
           <div className="icon">
             <SlGraph />
           </div>
-          <p className='text-icons'>Dashboard</p>
+          <p className="text-icons">Dashboard</p>
         </Link>
 
         <Link to="clients" className="container-icon">
           <div className="icon">
             <BsFillPeopleFill />
           </div>
-          <p className='text-icons'>Clientes</p>
+          <p className="text-icons">Clientes</p>
         </Link>
-
 
         <Link to="print" className="container-icon">
           <div className="icon">
             <AiOutlinePrinter />
           </div>
-          <p className='text-icons'>Imprimir</p>
+          <p className="text-icons">Imprimir</p>
         </Link>
 
         <Link to="configs" className="container-icon">
           <div className="icon">
-            <GoGear className='icon' />
+            <GoGear className="icon" />
           </div>
-          <p className='text-icons'>Configurações</p>
+          <p className="text-icons">Configurações</p>
         </Link>
-      </C.HeaderContainer >
+      </C.HeaderContainer>
     </C.ContainerHeader>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
