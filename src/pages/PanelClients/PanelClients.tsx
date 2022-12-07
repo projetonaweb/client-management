3638c9f916271b6cf204f6b23ff603e6c07648db
FOXXX <75998678+devrodrigomolina@users.noreply.github.com>
2022-12-06 23:29:39 -0300

    Create PanelClients.tsx

---
+++ src/pages/PanelClients/PanelClients.tsx
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import ClientsPanel from "../../components/ClientsPanel/ClientsPanel";
+import Header from "../../components/Header/Header";
+import { Title } from "../../components/Title/Title";
+import * as C from "./style";
+import { MdPersonAddAlt1 } from "react-icons/md";
+import { Inputs } from "../../components/Inputs/Inputs";
+import Modal from "../../components/ModalRegisterClients/ModalRegisterClients";
+import type { EventTypes } from "../../types/EventTypes";
+
+const PanelClients = () => {
+  const [modal, setModal] = useState(false);
+
+  const handleModal = ({ target }: EventTypes) => {
+    target.id == "overlay" && setModal(false);
+  };
+
+  return (
+    <>
+      {modal && <Modal onClick={handleModal} setModal={setModal} />}
+      <Header />
+      <C.ContainerClients>
+        <div className="client_panel">
+          <Title message="Clientes cadastrados" />
+          <div className="controls-area">
+            <Inputs type="text" placeholder="Pesquise seu cliente" />
+            <MdPersonAddAlt1 onClick={() => setModal(!modal)} />
+          </div>
+          <ClientsPanel />
+        </div>
+      </C.ContainerClients>
+    </>
+  );
+};
+
+export default PanelClients;
