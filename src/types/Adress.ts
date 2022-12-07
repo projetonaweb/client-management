701a036a888929ec475f149d7e6d70e07dce260a
FOXXX <75998678+devrodrigomolina@users.noreply.github.com>
2022-12-06 23:30:40 -0300

    Create Adress.ts

---
+++ src/types/Adress.ts
@@ -0,0 +1,13 @@
+export interface Adress {
+  id: number,
+  cep: string,
+  rua: string,
+  numero: string,
+  complemento: string,
+  bairro: string,
+  cidade: string,
+  estado: string,
+  cliente_id: number,
+  created_at: string,
+  updated_at: string
+}
