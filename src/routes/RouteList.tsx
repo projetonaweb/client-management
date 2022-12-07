bef4dfc4812c756b5cca88385e4fb8de1404d66a
FOXXX <75998678+devrodrigomolina@users.noreply.github.com>
2022-12-06 23:30:35 -0300

    Adicionado rota dinamica para cliente

--- src/routes/RouteList.tsx
+++ src/routes/RouteList.tsx
@@ -1,17 +1,17 @@
 import { useRoutes } from "react-router-dom"
-import App from "../App"
-import Clients from "../pages/Clients/Clients"
 import NotFound from "../pages/Error/NotFound"
 import Home from "../pages/Home/Home"
 import SingUp from "../pages/Register/SingUp"
 import { SingIn } from "../pages/SingIn/SingIn"
 import { RequireAuth } from "./RequireAuth"
-
+import PanelClients from "../pages/PanelClients/PanelClients"
+import Client from "../pages/Client/Client"
 
 export const RouteList = () => {
   return useRoutes([
     { path: '/', element: <RequireAuth><Home /></RequireAuth>},
-    { path: '/clients', element: <RequireAuth><Clients /></RequireAuth>},
+    { path: '/clients', element: <RequireAuth><PanelClients /></RequireAuth>},
+    { path: '/clients/:id', element: <RequireAuth><Client /></RequireAuth>},
     { path: '/login', element: <SingIn /> },
     { path: '/register', element: <SingUp /> },
     { path: '*', element: <NotFound /> }
